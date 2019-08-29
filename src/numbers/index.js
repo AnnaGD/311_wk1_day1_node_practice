bb622f6ecdc03a9ebb925a1e5949b342e75f1b31
Anna Goncharova <anna.gonch.liaison@gmail.com>
2019-08-29 18:35:11 -0500

    082719

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,20 +1,23 @@
-const isEven = (num) => {
-  // write code for numbers.isEven
+const isEven = num => {
+	return num % 2 === 0;
+};
 
-}
-
-const sum = (arr) => {
-  // write code for numbers.sum
-
-}
+const sum = arr => {
+	return arr.length === 0 ? 0 : arr[0] + sum(arr.slice(1));
+};
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
-}
+  let newSum = sumOf(arr)
+  if(newSum === num) {
+    return true
+  } else {
+    return false
+  }
+};
 
 module.exports = {
-  isEven,
-  sum,
-  comboSum
-}
+	isEven,
+	sum,
+	comboSum
+};
